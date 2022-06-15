967182f964de74ea7f04137df95724605ff1a659
victor <victor.barouh@gmail.com>
2022-06-16 00:03:11 +0200

    Update architecture

--- CHEH/Private/core/config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-define('PRIVATE',$_SERVER["DOCUMENT_ROOT"]."CHEH/private/");
-define('PRIVATE_VIEWS',$_SERVER["DOCUMENT_ROOT"]."CHEH/private/views/");
-define('PRIVATE_CONTROLLERS',$_SERVER["DOCUMENT_ROOT"]."CHEH/private/controllers/");
-define('PRIVATE_MODELS',$_SERVER["DOCUMENT_ROOT"]."CHEH/private/models/");
---
+++ Private/views/corbackgame.view.php
@@ -0,0 +1,8 @@
+<style type="text/css"></style>
+
+<div>
+	<img src="">
+
+
+</div>
+
